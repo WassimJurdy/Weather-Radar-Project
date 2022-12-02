871e404d719700b37dd7411d5f93faa8809fc3fb
wassim <wassim.eljurdy@hotmail.com>
2022-12-02 12:35:53 +0200

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "91bbe5adcdmsh32aa4b4abbc6eb9p13c2dfjsn4fbc9c2ccd40",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const API_KEY = "5bfe1ec0445bd9d734959cbb1f73c32b";
+
